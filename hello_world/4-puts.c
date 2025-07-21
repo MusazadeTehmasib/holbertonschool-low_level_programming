9b657894191aa58e11e05c8ba0273db3a2b55b5c
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-21 12:44:30 +0000

    heh

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - bla bla
+ *
+ * Description: blablabla
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
