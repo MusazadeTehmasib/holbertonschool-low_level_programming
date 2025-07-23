9313f2c32989c48ff12461a91b1d4889b5343437
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-23 08:47:06 +0000

    res

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,14 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Entry point
+ * main - idk
  *
- * Return: Always 0 (Success)
+ * Return: idk
  */
 int main(void)
 {
     int n;
-
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
