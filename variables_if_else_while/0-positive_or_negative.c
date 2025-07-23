d535d3aea38ee6739dd321f850c24c65ee780121
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-23 09:58:47 +0000

    work

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - pls work`
  *
@@ -25,6 +26,7 @@ int main(void)
 	{
 		printf("%d is negative\n", n);
 	}
+
 	return (0);
 }
 
---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * words
+ */
+int main(void)
+{
+	int n;
+	int nl;
+
+	srand(time(0));
+	n = rand() -RAND_MAX / 2;
+	nl = n % 10;
+	printf("Last digit of %d is %d\n", n, nl);
+	if (nl > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	if (nl == 0)
+	{
+		printf("and is 0\n");
+	}else if(nl < 6)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	return (0);
+}
