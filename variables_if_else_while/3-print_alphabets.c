33d29578464b07a5531f5c08470660dcf5150648
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-24 14:29:45 +0000

    bruh

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
 /**
- * main - woke
+ * main - bruh
  *
- * Return: woke
+ * Return: bruh
  */
 int main(void)
 {
     char lower = 'a';
     char upper = 'A';
 
-    /* Print lowercase alphabet */
     while (lower <= 'z')
     {
         putchar(lower++);
     }
 
-    /* Print uppercase alphabet */
     while (upper <= 'Z')
     {
         putchar(upper++);
     }
 
-    /* Print a newline */
     putchar('\n');
 
     return (0);
