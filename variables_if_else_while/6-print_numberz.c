3370f2d720ca95cca66dcc2a41c074b00ec294e4
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-25 12:01:38 +0000

    haha

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - nums print 1-9
+ *
+ * Return: always0 baby
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
