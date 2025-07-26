e22f9b231f850d63cabc5e4e1b648df3eba4cbc0
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-07-26 12:59:23 +0000

    joojo

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - i will kill betty
+ *
+ * Description: baldnsadasjd
+ */
+int _islower(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
