50405165cf546f98155262778588f4140e3346b6
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 11:17:23 +0000

    jojo

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	char nums[] = "0123456789\n";
+	int i = 0;
+
+	while (nums[i] != '\0')
+	{
+		_putchar(nums[i]);
+		i++;
+	}
+}
