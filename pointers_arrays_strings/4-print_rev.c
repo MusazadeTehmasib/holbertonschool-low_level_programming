c3d6250223be7a1bc76e6443355c8d17bf8fd886
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 13:06:30 +0000

    fof

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to print in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (len > 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+
+	_putchar('\n');
+}
