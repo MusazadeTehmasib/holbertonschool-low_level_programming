d7b48b043680dc98099b21acc8304a4da91dbc29
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 12:03:32 +0000

    w

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,12 @@ void print_line(int n)
 
     if (n <= 0)
     {
-        _putchar('\n');
-        return;
+	_putchar('\n');
+	return;
     }
 
     for (i = 0; i < n; i++)
-        _putchar('_');
+	_putchar('_');
 
     _putchar('\n');
 }
