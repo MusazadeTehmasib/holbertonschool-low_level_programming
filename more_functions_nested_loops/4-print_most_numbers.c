39d5c44c890c106ae7b64842b21c8610bf1d02ec
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 12:34:52 +0000

    ee

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,20 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers from 0 to 9 except 2 and 4
- * followed by a new line
+ * print_most_numbers - prints numbers from 0 to 9 excluding 2 and 4,
+ *                      followed by a new line.
+ *
+ * You can only use _putchar twice in the entire function.
  */
 void print_most_numbers(void)
 {
-	char nums[] = "01356789\n";
 	int i;
 
-	for (i = 0; nums[i] != '\0'; i++)
-		_putchar(nums[i]);
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
