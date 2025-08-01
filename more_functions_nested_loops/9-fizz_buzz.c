9b9c9629bd7635cf22e63800c927bd15a9a743ce
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 12:28:09 +0000

    dd

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,27 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 			continue;
 		}
 
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			continue;
 		}
 
 		if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 			continue;
 		}
 
 		printf("%d ", i);
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
 	return 0;
 }
