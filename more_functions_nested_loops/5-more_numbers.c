26af47dfb2d6ed072c887f6c0a793c33bfee1276
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-01 11:49:25 +0000

    dd

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,8 +10,10 @@ void more_numbers(void)
 	int i;
 	int z;
 
-	for (z = 0; z < 10; z++){
-		for (i = 0; nums[i] != '\0'; i++){
+	for (z = 0; z < 10; z++)
+	{
+		for (i = 0; nums[i] != '\0'; i++)
+		{
 			_putchar(nums[i]);
 		}
 	}
