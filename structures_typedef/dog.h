a8eb56cec8cb1d4090bbd3db8e61d03b49c592b0
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-16 17:18:47 +0000

    d

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h> /* For NULL */
+
 /**
  * struct dog - structure for a dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
 struct dog
 {
 	char *name;
@@ -20,5 +16,13 @@ struct dog
 	char *owner;
 };
 
+/* Typedef for convenience */
+typedef struct dog dog_t;
+
+/* Function prototypes */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
