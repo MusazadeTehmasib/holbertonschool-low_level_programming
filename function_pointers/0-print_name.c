24e145dc827f4f54adeac9dd3ed72b0bfd4af0fb
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-20 13:30:03 +0000

    d

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ * print_name - prints
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
 void print_name(char *name, void (*f)(char *)){
-		
 	if (name != NULL && f != NULL)
 		f(name);
 
