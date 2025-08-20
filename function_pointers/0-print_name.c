651060d553317f26d10365099e2a8b1605ded6fa
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-20 13:26:25 +0000

    d

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,15 +4,6 @@
 void print_name(char *name, void (*f)(char *)){
 		
 	if (name != NULL && f != NULL)
-		printf("%s\n", name);
+		f(name);
 
-}
-
-void print_name_uppercase(char *name){
-	int i = 0;
-
-	printf("Hello, my name is ");
-		for (i = 0; name[i] != '\0'; i++){
-			putchar(toupper(name[i]));
-	}
 }
