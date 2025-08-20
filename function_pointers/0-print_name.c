7c27a9805e43c7a45d9581d8b067f1e0d27c1eb8
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-20 13:02:41 +0000

    d

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 
 void print_name(char *name, void (*f)(char *)){
+	int i = 0;
+	
 	if (name != NULL && f != NULL)
 		printf("%s\n", name);
 	
-	int i = 0;
+	
 	if (name != NULL && f == NULL){
 		while (name[i]){
 			putchar(toupper(name[i]));
-			int++;	
+			i++;	
 		}
 	}
 }
