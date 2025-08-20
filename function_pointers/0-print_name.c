575aa4513b2be9354baf97aedddf7b2376b51200
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-20 13:17:09 +0000

    d

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,16 +2,17 @@
 #include <ctype.h>
 
 void print_name(char *name, void (*f)(char *)){
-	int i = 0;
-	
+		
 	if (name != NULL && f != NULL)
-		f("%s\n", name);
-	
-	
-	void print_uppercase(char *name){
-		printf("Hello, my name is ");
+		printf("%s\n", name);
+
+}
+
+void print_uppercase(char *name){
+	int i = 0;
+
+	printf("Hello, my name is ");
 		for (i = 0; name[i] != '\0'; i++){
 			putchar(toupper(name[i]));
-		}
 	}
 }
