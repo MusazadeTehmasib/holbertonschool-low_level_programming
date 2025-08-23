144ab24b1e2d2cf915d75a227d9074f037197fd0
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-23 15:54:38 +0000

    d

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,134 +1,53 @@
-#include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
 /**
- * _puts - prints a string using _putchar
- * @s: string
+ * print_all - prints anything based on a format string
+ * @format: list of types of arguments
+ *
+ * c: char
+ * i: integer
+ * f: float
+ * s: string (print (nil) if NULL)
  */
-void _puts(char *s)
-{
-	while (s && *s)
-	{
-		_putchar(*s);
-		s++;
-	}
-}
 
-/**
- * _print_number - prints an integer using _putchar
- * @n: integer
- */
-void _print_number(int n)
-{
-	unsigned int num;
 
-	if (n < 0) /* <-- the only if in the file */
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-	{
-		num = n;
-	}
-
-	if (num / 10)
-		_print_number(num / 10);
-	_putchar((num % 10) + '0');
-}
-
-/**
- * print_float - prints a float using _putchar
- * @f: float number
- */
-void print_float(double f)
-{
-	int int_part = (int)f;
-	double frac = f - int_part;
-	int frac_part;
-
-	/* handle negative zero using switch */
-	switch (f < 0 && int_part == 0)
-	{
-		case 1:
-			_putchar('-');
-			break;
-		default:
-			break;
-	}
-
-	_print_number(int_part);
-	_putchar('.');
-
-	/* make fraction positive without if */
-	switch (frac < 0)
-	{
-		case 1:
-			frac = -frac;
-			break;
-		default:
-			break;
-	}
-
-	frac_part = (int)(frac * 1000000);
-	_print_number(frac_part);
-}
-
-/**
- * print_all - prints anything
- * @format: list of argument types
- */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, sep = 0;
-	char *s;
+	char *str;
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
 		if (sep)
+			printf(", ");
+		if (format[i] == 'c')
 		{
-			_putchar(',');
-			_putchar(' ');
+			printf("%c", va_arg(args, int));
+			sep = 1;
 		}
-
-		switch (format[i])
+		if (format[i] == 'i')
 		{
-			case 'c':
-				_putchar(va_arg(args, int));
-				sep = 1;
-				break;
-			case 'i':
-				_print_number(va_arg(args, int));
-				sep = 1;
-				break;
-			case 'f':
-				print_float(va_arg(args, double));
-				sep = 1;
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				switch (s == NULL)
-				{
-					case 1:
-						_puts("(nil)");
-						sep = 1;
-						break;
-					default:
-						_puts(s);
-						sep = 1;
-						break;
-				}
-				break;
-			default:
-				sep = 0;
-				break;
+			printf("%d", va_arg(args, int));
+			sep = 1;
+		}
+		if (format[i] == 'f')
+		{
+			printf("%f", va_arg(args, double));
+			sep = 1;
+		}
+		if (format[i] == 's')
+		{
+			str = va_arg(args, char *);
+			if (!str)
+				str = "(nil)";
+			printf("%s", str);
+			sep = 1;
 		}
 		i++;
 	}
-	_putchar('\n');
+	printf("\n");
 	va_end(args);
 }
