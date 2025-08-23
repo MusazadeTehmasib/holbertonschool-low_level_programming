0079b5e55afd6f0c8ebdb3825eb15ea895049308
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-23 16:18:16 +0000

    d

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,55 +4,36 @@
 /**
  * print_all - prints anything based on a format string
  * @format: list of types of arguments
- *
- * c: char
- * i: integer
- * f: float
- * s: string (print (nil) if NULL)
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	int i = 0, sep = 0;
 	char *str;
-	char valid;
+	const char *p;
 
 	va_start(args, format);
+	p = format;
 
-	while (format && format[i])
+	while (p && *p)
 	{
-		valid = 1;
-		switch (format[i])
+		/* Only one if for valid separator */
+		if (*p == 'c' || *p == 'i' || *p == 'f' || *p == 's')
 		{
-			case 'c':
-				if (sep)
-					printf(", ");
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				if (sep)
-					printf(", ");
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				if (sep)
-					printf(", ");
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				if (sep)
-					printf(", ");
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(nil)";
-				printf("%s", str);
-				break;
-			default:
-				valid = 0; /* ignore invalid types */
+			if (sep)
+				printf(", ");
+
+			/* Print value based on type */
+			(*p == 'c') && printf("%c", va_arg(args, int));
+			(*p == 'i') && printf("%d", va_arg(args, int));
+			(*p == 'f') && printf("%f", va_arg(args, double));
+			(*p == 's') &&
+				(str = va_arg(args, char *)) &&
+				printf("%s", str ? str : "(nil)");
+
+			sep = 1;
 		}
-		if (valid)
-			sep = 1; /* only update sep for valid types */
-		i++;
+		p++;
 	}
 
 	printf("\n");
