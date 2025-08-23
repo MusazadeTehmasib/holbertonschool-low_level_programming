d609a2de1cb55639a7399b331df81c84e2d5312c
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-23 15:45:38 +0000

    d

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,33 +6,21 @@
  * _puts - prints a string using _putchar
  * @s: string
  */
-
-
 void _puts(char *s)
 {
 	while (s && *s)
-	{
-		_putchar(*s);
-		s++;
-	}
+		_putchar(*s++);
 }
 
 /**
  * _print_number - prints an integer using _putchar
  * @n: integer
  */
-
 void _print_number(int n)
 {
-	unsigned int num;
+	unsigned int num = (n < 0) ? -n : n;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -n;
-	}
-	else
-		num = n;
+	(n < 0) && _putchar('-');
 
 	if (num / 10)
 		_print_number(num / 10);
@@ -43,23 +31,19 @@ void _print_number(int n)
  * print_float - prints a float using _putchar
  * @f: float number
  */
-
 void print_float(double f)
 {
 	int int_part = (int)f;
 	double frac = f - int_part;
 	int frac_part;
 
-	if (f < 0 && int_part == 0)
-		_putchar('-');
+	(f < 0 && int_part == 0) && _putchar('-');
 
 	_print_number(int_part);
 	_putchar('.');
 
-	if (frac < 0)
-		frac *= -1;
-
-	frac_part = (int)(frac * 1000000); /* 6 decimal places */
+	frac = (frac < 0) ? -frac : frac;
+	frac_part = (int)(frac * 1000000);
 	_print_number(frac_part);
 }
 
@@ -67,7 +51,6 @@ void print_float(double f)
  * print_all - prints anything
  * @format: list of argument types
  */
-
 void print_all(const char * const format, ...)
 {
 	va_list args;
@@ -78,32 +61,22 @@ void print_all(const char * const format, ...)
 
 	while (format && format[i])
 	{
-		if (sep)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		sep && (_putchar(','), _putchar(' '));
 
 		switch (format[i])
 		{
 			case 'c':
-				_putchar(va_arg(args, int));
-				sep = 1;
+				_putchar(va_arg(args, int)), sep = 1;
 				break;
 			case 'i':
-				_print_number(va_arg(args, int));
-				sep = 1;
+				_print_number(va_arg(args, int)), sep = 1;
 				break;
 			case 'f':
-				print_float(va_arg(args, double));
-				sep = 1;
+				print_float(va_arg(args, double)), sep = 1;
 				break;
 			case 's':
 				s = va_arg(args, char *);
-				if (!s)
-					s = "(nil)";
-				_puts(s);
-				sep = 1;
+				_puts(s ? s : "(nil)"), sep = 1;
 				break;
 			default:
 				sep = 0;
