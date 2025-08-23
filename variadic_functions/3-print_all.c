c79035f56de2889e2d21911387d162f5a064597e
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-08-23 16:07:09 +0000

    d

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,35 +17,44 @@ void print_all(const char * const format, ...)
 	char *str;
 
 	va_start(args, format);
+
 	while (format && format[i])
 	{
-		if (sep)
-			printf(", ");
-
 		switch (format[i])
 		{
 			case 'c':
+				if (sep)
+					printf(", ");
 				printf("%c", va_arg(args, int));
 				sep = 1;
 				break;
 			case 'i':
+				if (sep)
+					printf(", ");
 				printf("%d", va_arg(args, int));
 				sep = 1;
 				break;
 			case 'f':
+				if (sep)
+					printf(", ");
 				printf("%f", va_arg(args, double));
 				sep = 1;
 				break;
 			case 's':
+				if (sep)
+					printf(", ");
 				str = va_arg(args, char *);
-				printf("%s", str ? str : "(nil)");
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
 				sep = 1;
 				break;
 			default:
-				break;
+				break; /* ignore invalid characters */
 		}
 		i++;
 	}
+
 	printf("\n");
 	va_end(args);
 }
