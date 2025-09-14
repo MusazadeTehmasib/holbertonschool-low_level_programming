a1ac858071983bbf04fa5df1aa42a33663c39975
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-09-14 01:17:47 +0000

    d

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,14 +25,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 	for (temp = ht->array[index]; temp; temp = temp->next)
-	{
 		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
 			temp->value = val_copy;
 			return (1);
 		}
-	}
 
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
