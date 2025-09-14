7df7aac297fc666e7d37095a172927338c69d6a8
MusazadeTehmasib <tehmasibmusazade@email.com>
2025-09-14 01:13:03 +0000

    d

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,62 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: pointer to the hash table
+ * @key: the key (cannot be an empty string)
+ * @value: the value associated with the key (will be duplicated)
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node, *temp;
+	char *value_copy;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = value_copy;
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+	{
+		free(value_copy);
+		return (0);
+	}
+
+	new_node->key = strdup(key);
+	if (!new_node->key)
+	{
+		free(value_copy);
+		free(new_node);
+		return (0);
+	}
+
+	new_node->value = value_copy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
